a0ef98ccfe5c02851a3f6faa20f00359a71df0de
Thiago Andrade Silva <thiago@contasimples.com>
2021-08-07 10:01:32 -0300

    fix: adjust broken code

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -105,8 +105,8 @@ describe('SignUp Controller', () => {
       body: {
         name: 'any_name',
         email: 'invalid_email@email.com',
-        password: 'any_passwod',
-        passwordConfirmation: 'any_passdowrd'
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
@@ -121,8 +121,8 @@ describe('SignUp Controller', () => {
       body: {
         name: 'any_name',
         email: 'any_email@email.com',
-        password: 'any_passwod',
-        passwordConfirmation: 'any_passdowrd'
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
       }
     }
     sut.handle(httpRequest)
@@ -138,8 +138,8 @@ describe('SignUp Controller', () => {
       body: {
         name: 'any_name',
         email: 'any_email@email.com',
-        password: 'any_passwod',
-        passwordConfirmation: 'any_passdowrd'
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
