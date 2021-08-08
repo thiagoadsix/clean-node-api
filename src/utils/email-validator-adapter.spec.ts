d1d455d2dfa19484588e2426b80bb00b79e54edf
Thiago Andrade Silva <thiago@contasimples.com>
2021-08-08 01:49:37 -0300

    refactor: move sut creation to a factory helper method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,5 @@
 import validator from 'validator'
+import { EmailValidator } from '../presentation/protocols/emailValidator'
 import { EmailValidatorAdapter } from './email-validator'
 
 jest.mock('validator', () => ({
@@ -7,22 +8,26 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidator => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adaptar', () => {
   test('Should return false if validator returns false ', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@email.com')
     expect(isValid).toBe(false)
   })
 
   test('Should return true if validator returns true ', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@email.com')
     expect(isValid).toBe(true)
   })
 
   test('Should call validator with correct email ', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@email.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@email.com')
