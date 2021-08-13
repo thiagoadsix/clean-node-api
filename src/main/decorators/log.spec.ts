1a84d616d593b7a5ffe533ee44bc5a9c8fe7ad05
Thiago Andrade Silva <thiago@contasimples.com>
2021-08-13 08:30:43 -0300

    test: ensure log controller decorator return the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -49,4 +49,26 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toBeCalledWith(httpRequest)
   })
+
+  test('Should the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Thiago',
+        email: 'thiago@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    })
+  })
 })
