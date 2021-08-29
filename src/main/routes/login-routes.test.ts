89da9af2826d15578b4b7edcd9b360292392aa82
Thiago Andrade Silva <thiago@contasimples.com>
2021-08-29 16:12:47 -0300

    test: add LoginRoute

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,11 @@
+import { Collection } from 'mongodb'
 import request from 'supertest'
+import { hash } from 'bcrypt'
 import app from '../config/app'
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -13,7 +17,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -26,6 +30,24 @@ describe('Login Routes', () => {
           email: 'thiago@email.com',
           password: '123456',
           passwordConfirmation: '123456'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on success', async () => {
+      const password = await hash('12345', 12)
+      await accountCollection.insertOne({
+        name: 'Thiago',
+        email: 'thiago@email.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'thiago@email.com',
+          password: '12345'
         })
         .expect(200)
     })
