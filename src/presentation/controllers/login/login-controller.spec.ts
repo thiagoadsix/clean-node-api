0f2e0ac83bc3537891ae2a6aaccf383e9f4a8126
Thiago Andrade <thiago.adsix@gmail.com>
2021-09-21 08:58:08 -0300

    test: ensure SignUpController calls Authenticatino with correct values

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -58,18 +58,21 @@ describe('Login Controller', () => {
     await sut.handle(makeFakeRequest())
     expect(constAuthSpy).toHaveBeenCalledWith({ email: 'any_email@email.com', password: 'any_password' })
   })
+
   test('Should return 401 if invalid credentials are provided', async () => {
     const { sut, authenticationStub } = makeSut()
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(unauthorized())
   })
+
   test('Should return 500 if Authentication throws', async () => {
     const { sut, authenticationStub } = makeSut()
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
   })
+
   test('Should return 200 if valid credentials are provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
@@ -77,6 +80,7 @@ describe('Login Controller', () => {
       accessToken: 'any_token'
     }))
   })
+
   test('Should call Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
@@ -84,6 +88,7 @@ describe('Login Controller', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
+
   test('Should return 400 if validation returns an error', async () => {
     const { sut, validationStub } = makeSut()
     jest.spyOn(validationStub, 'validate').mockReturnValue(new MissingParamError('any_field'))
